bd19e0d28456050c7f835e67069ffaf2168acdb0
intermundos <junk.intermundos@gmail.com>
2016-11-22 17:16:56 +0200

    Added pager

--- app/components/posts/PostsView.js
+++ app/components/posts/PostsView.js
@@ -1,31 +1,71 @@
 import React, { Component } from 'react';
 import BlogEntry        from './BlogEntry';
 
+import chunk        from 'lodash/chunk';
+
 class PostsView extends Component {
 
-  render(){
-    const { posts } = this.props;
+	constructor(props){
+	    super(props);
 
-    const blogEntriesList = posts.visiblePosts.map((post, index)=>(<BlogEntry post={ post } key={ index } />));
+	    this.renderPosts = this.renderPosts.bind(this);
+	    this.loadNewer   = this.loadNewer.bind(this);
+	    this.loadOlder   = this.loadOlder.bind(this);
 
-    if (posts.visiblePosts.length === 0) {
-    	return (
-			<section className="col-md-8">
-				<h1 className="page-header">Nothing to show...</h1>
-			</section>
+	    this.state = {
+			pageIndex: 0,
+			chunked: chunk(this.props.posts.visiblePosts, 3)
+	    }
+	}
+
+	renderPosts(){
+		const postsToShow = this.state.chunked[this.state.pageIndex];
+		return (
+			postsToShow.map((post, index)=>(<BlogEntry post={ post } key={ index } />))
 		)
 	}
-    return (
 
-      <section className="col-md-8">
+	loadOlder(){
+		window.scrollTo(0, 0);
+		this.setState({
+			pageIndex: this.state.pageIndex + 1
+		});
+	}
 
-		  <h1 className="page-header">Showing { posts.visiblePosts.length } Posts</h1>
+	loadNewer(){
+		window.scrollTo(0, 0);
+		this.setState({
+			pageIndex: this.state.pageIndex - 1
+		});
+	}
 
-		  { blogEntriesList }
+	render(){
+		return (
 
-      </section>
-    )
-  }
+		  <section className="col-md-8">
+
+			  <h1 className="page-header">Showing { this.props.posts.visiblePosts.length } Posts</h1>
+
+			  { this.renderPosts() }
+
+			  <ul className="pager">
+				  { this.state.pageIndex == this.state.chunked.length-1 ?
+					  null :
+					  <li className="previous">
+						  <a onClick={ this.loadOlder } className="pager-control pager-older">← Older</a>
+					  </li>
+
+				  }
+				  { this.state.pageIndex == 0 ?
+					  null :
+					  <li className="next">
+						  <a onClick={ this.loadNewer } className="pager-control pager-newer"> Newer →</a>
+					  </li>
+				  }
+			  </ul>
+		  </section>
+		)
+	}
 }
 
 export default PostsView;
